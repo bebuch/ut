3b05ddce9ee449f8d194c02e2e8a2abf0dc4d4a1
Benjamin Buch <benjamin.buch@tu-ilmenau.de>
2022-03-30 18:24:27 +0200

    let test fail if any subcondition evaluates to false

--- experimental/main.cpp
+++ experimental/main.cpp
@@ -183,6 +183,14 @@ namespace ut{
     struct value: basic_value<T>{
         using basic_value<T>::basic_value;
 
+        [[nodiscard]] static consteval bool is_expected_only()noexcept{
+            return false;
+        }
+
+        [[nodiscard]] static consteval bool all_conditions_true()noexcept{
+            return true;
+        }
+
         value(detail::constant<T> const& v) = delete;
 
         void print(std::ostream& os, bool const pass)const{
@@ -194,6 +202,14 @@ namespace ut{
     template <typename T>
     struct expected: basic_value<T>{
         using basic_value<T>::basic_value;
+
+        [[nodiscard]] static consteval bool is_expected_only()noexcept{
+            return true;
+        }
+
+        [[nodiscard]] static consteval bool all_conditions_true()noexcept{
+            return true;
+        }
 
         constexpr expected(detail::constant<T> const& v)noexcept
             : basic_value<T>(v())
@@ -254,14 +270,16 @@ namespace ut{
 
     template <concepts::expression Expr>
     void expect(Expr const& expr){
+        static_assert(!Expr::is_expected_only(), "Your condition dosn't contain any test values.");
+
         auto const flags(std::cout.flags());
         std::cout << std::boolalpha;
-        auto const expectation = static_cast<bool>(expr());
+        auto const pass = expr.all_conditions_true();
         std::cout
-            << detail::format::color(expectation)
-            << (expectation ? "pass"sv : "fail"sv)
+            << detail::format::color(pass)
+            << (pass ? "pass"sv : "fail"sv)
             << detail::format::reset << ": ";
-        expr.print(std::cout, expectation);
+        expr.print(std::cout, pass);
         std::cout << "\n";
         std::cout.flags(flags);
     }
@@ -275,18 +293,49 @@ namespace ut{
             , b_(detail::to_expression(b))
             {}
 
+        static consteval bool is_expected_only()noexcept{
+            return A::is_expected_only() && B::is_expected_only();
+        }
+
+        [[nodiscard]] bool all_conditions_true()const{
+            return a_.all_conditions_true() && b_.all_conditions_true() && (is_expected_only() ||
+                [this]{
+                    if constexpr(std::is_same_v<std::remove_cvref_t<decltype((*this)())>, bool>){
+                        return (*this)();
+                    }else{
+                        return true;
+                    }
+                }());
+        }
+
         [[nodiscard]] constexpr decltype(auto) operator()()const{
             return a_() == b_();
         }
 
-        void print(std::ostream& os, bool const)const{
-            auto const pass = static_cast<bool>((*this)());
+        void print(std::ostream& os, bool const pass)const{
+            bool const sub_pass =
+                [this, pass]{
+                    if constexpr(std::is_same_v<std::remove_cvref_t<decltype((*this)())>, bool>){
+                        return (*this)();
+                    }else{
+                        return pass;
+                    }
+                }();
             os << "(";
-            a_.print(os, pass);
-            os << (pass ? detail::format::pass : detail::format::fail) << " == " << detail::format::reset;
-            b_.print(os, pass);
+            a_.print(os, sub_pass);
+            os << (sub_pass ? detail::format::pass : detail::format::fail) << " == " << detail::format::reset;
+            b_.print(os, sub_pass);
             os << " => ";
-            os << detail::format::sum << pass << detail::format::reset;
+            auto const to_expression =
+                [](concepts::non_expression auto const& v){
+                    if constexpr(is_expected_only()){
+                        return expected(v);
+                    }else{
+                        return value(v);
+                    }
+                };
+
+            to_expression((*this)()).print(os, sub_pass);
             os << ")";
         }
 
@@ -427,5 +476,7 @@ int main(){
     ut::expect(ut::value("tested"sv) == ut::value("tested"sv));
     ut::expect(ut::expected("reference"sv) == ut::value("tested"sv) == ut::value(true));
     ut::expect(ut::equal("tested"sv, "tested"sv) == ut::value(false));
+    ut::expect(ut::expected("reference"sv) == ut::value("tested"sv) == ut::value(false));
+    ut::expect(ut::expected("reference"sv) == ut::expected("tested"sv) == ut::value(false));
     ut::expect(ut::expected(1) == ut::value(2));
 }
